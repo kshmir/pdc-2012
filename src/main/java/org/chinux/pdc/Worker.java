7de877c054b5c19a27beb9da20f4feb1e9d9c91b
Cristian Pereyra <criis.pereyra@gmail.com>
2012-04-23 14:34:45 -0300

    added log4j

--- src/main/java/org/chinux/pdc/Worker.java
+++ src/main/java/org/chinux/pdc/Worker.java
@@ -25,6 +25,7 @@ public abstract class Worker<T extends DataEvent> implements Runnable {
 	/**
 	 * This loop receives all the data and handles all the bussiness logic.
 	 */
+	@Override
 	public void run() {
 		T dataEvent;
 
