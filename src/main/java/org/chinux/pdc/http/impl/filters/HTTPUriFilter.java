39984959cf13724b7976ffc4e9bb6f245ebeac3c
Federico Homovc <federicohomovc@gmail.com>
2012-06-04 18:10:46 -0300

    regex compiler exception catched

--- src/main/java/org/chinux/pdc/http/impl/filters/HTTPUriFilter.java
+++ src/main/java/org/chinux/pdc/http/impl/filters/HTTPUriFilter.java
@@ -4,20 +4,33 @@ import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
+import org.apache.log4j.Logger;
 import org.chinux.pdc.http.api.HTTPFilter;
 import org.chinux.pdc.http.util.ErrorPageProvider;
 import org.chinux.pdc.workers.impl.HTTPProxyEvent;
+import org.chinux.pdc.workers.impl.HTTPResponseEventHandler;
 
 public class HTTPUriFilter implements HTTPFilter {
+
+	static Logger log = Logger.getLogger(HTTPResponseEventHandler.class);
 
 	@Override
 	public boolean isValid(final HTTPProxyEvent event) {
 		final List<String> uris = event.getEventConfiguration()
 				.getBlockedURLs();
 		final String req = event.getRequest().getHeaders().getHeader("Host");
-		for (final String uri : uris) {
-			final Pattern pattern = Pattern.compile(uri);
+		for (String uri : uris) {
+			Pattern pattern;
+			uri += ")";
+			uri = "(" + uri;
+			try {
+				pattern = Pattern.compile(uri);
+			} catch (final PatternSyntaxException e) {
+				log.error("Invalid regular expresion");
+				return true;
+			}
 			final Matcher match = pattern.matcher(req);
 			if (match.find()) {
 				return false;
